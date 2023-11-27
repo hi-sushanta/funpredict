490c7bf70d3641b2952e3b7885cb3bf5b89e015d
chi <iamchi@skiff.com>
2023-11-27 13:59:25 +0530

    Added one Base class and inherit them both class to using loss_plot() function.

--- funpredict/torch_train.py
+++ funpredict/torch_train.py
@@ -4,9 +4,33 @@ from tqdm import tqdm
 from torch import optim
 import matplotlib.pyplot as plt
 
+class BaseClass:
+    def __init__(self):
+        self.fig = None 
+        self.ax = None 
+        pass 
+        
+    def loss_plot(self,epoch:int,loss:float,label_loss:str,last_epoch:int,sign:str):
+         # Create a new figure if needed
+        if self.fig is None:
+            self.fig, self.ax = plt.subplots()
 
-class TrainClassifer:
-    def __init__(self,model,dataloader,loss,opt,lr=0.002,device='cpu',):
+        # Add the current loss to the plot
+        self.fig.suptitle("Model Tracking")
+        self.ax.set_xlabel('Epoch')
+        self.ax.set_ylabel(label_loss)
+        self.ax.plot(epoch, loss, sign)
+
+        # Update the plot
+        plt.draw()
+        plt.pause(0.01)
+        if epoch+1 == last_epoch:
+            plt.show()
+
+
+class TrainClassifer(BaseClass):
+    def __init__(self,model,dataloader,loss,opt,lr=0.002,device='cpu'):
+        super().__init__()
         self.model  = model 
         self.dataloader = dataloader
         self.lr = lr
@@ -15,8 +39,6 @@ class TrainClassifer:
         self.tracking_loss = []
         self.tracking_ep = []
         self.device=device
-        self.fig = None 
-        self.ax = None
     def train(self,epoch:int=100,label_loss:str="Loss",
               sign:str="o"):
         self.fig=None 
@@ -37,26 +59,10 @@ class TrainClassifer:
             self.loss_plot(i,self.tracking_loss[i],label_loss=label_loss,last_epoch=100,sign=sign)
         return self.model
     
-    def loss_plot(self,epoch:int,loss:float,label_loss:str,last_epoch:int,sign:str):
-         # Create a new figure if needed
-        if self.fig is None:
-            self.fig, self.ax = plt.subplots()
 
-        # Add the current loss to the plot
-        self.fig.suptitle("Model Tracking")
-        self.ax.set_xlabel('Epoch')
-        self.ax.set_ylabel(label_loss)
-        self.ax.plot(epoch, loss, sign)
-
-        # Update the plot
-        plt.draw()
-        plt.pause(0.01)
-        if epoch+1 == last_epoch:
-            plt.show()
-
-
-class TrainRegressor:
+class TrainRegressor(BaseClass):
     def __init__(self,model,dataloader,loss,opt,lr=0.002,device='cpu'):
+        super().__init__()
         self.model  = model 
         self.dataloader = dataloader
         self.lr = lr
@@ -87,24 +93,6 @@ class TrainRegressor:
             self.tracking_ep.append(i+1)
             self.loss_plot(i,self.tracking_loss[i],label_loss=label_loss,last_epoch=100,sign=sign)
         return self.model
-    
-    def loss_plot(self,epoch:int,loss:float,label_loss:str,last_epoch:int,sign:str):
-         # Create a new figure if needed
-        if self.fig is None:
-            self.fig, self.ax = plt.subplots()
-
-        # Add the current loss to the plot
-        self.fig.suptitle("Model Tracking")
-        self.ax.set_xlabel('Epoch')
-        self.ax.set_ylabel(label_loss)
-        self.ax.plot(epoch, loss, sign)
-
-        # Update the plot
-        plt.draw()
-        plt.pause(0.01)
-        if epoch+1 == last_epoch:
-            plt.show()
-    
 
 
 
